820662d98a054f8604a8f42e9cd50d0cff74eee4
Юлия Агафонова <uliaagafonova6143@gmail.com>
2024-12-23 13:47:09 +0400

    Add files via upload

---
+++ AccountForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace NeedleWorkShop2
+{
+    partial class AccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccountForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CustomerButton = new System.Windows.Forms.Button();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.PatronymicTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.AddressTextBox = new System.Windows.Forms.TextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.OrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(338, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Мои данные";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(67, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(110, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(56, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Отчество";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(61, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 46);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "   Номер\r\nтелефона";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(99, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Адрес";
+            // 
+            // CustomerButton
+            // 
+            this.CustomerButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CustomerButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.CustomerButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CustomerButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CustomerButton.Location = new System.Drawing.Point(366, 377);
+            this.CustomerButton.Name = "CustomerButton";
+            this.CustomerButton.Size = new System.Drawing.Size(127, 41);
+            this.CustomerButton.TabIndex = 13;
+            this.CustomerButton.Text = "Сохранить";
+            this.CustomerButton.UseVisualStyleBackColor = false;
+            this.CustomerButton.Click += new System.EventHandler(this.CustomerButton_Click);
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LastNameTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LastNameTextBox.Location = new System.Drawing.Point(221, 112);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(402, 27);
+            this.LastNameTextBox.TabIndex = 17;
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.FirstNameTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FirstNameTextBox.Location = new System.Drawing.Point(221, 159);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(402, 27);
+            this.FirstNameTextBox.TabIndex = 18;
+            // 
+            // PatronymicTextBox
+            // 
+            this.PatronymicTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PatronymicTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PatronymicTextBox.Location = new System.Drawing.Point(221, 205);
+            this.PatronymicTextBox.Name = "PatronymicTextBox";
+            this.PatronymicTextBox.Size = new System.Drawing.Size(402, 27);
+            this.PatronymicTextBox.TabIndex = 19;
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PhoneNumberTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(221, 254);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(402, 27);
+            this.PhoneNumberTextBox.TabIndex = 20;
+            // 
+            // AddressTextBox
+            // 
+            this.AddressTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.AddressTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddressTextBox.Location = new System.Drawing.Point(221, 303);
+            this.AddressTextBox.Name = "AddressTextBox";
+            this.AddressTextBox.Size = new System.Drawing.Size(402, 27);
+            this.AddressTextBox.TabIndex = 21;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ExitButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ExitButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ExitButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExitButton.Location = new System.Drawing.Point(695, 377);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(127, 41);
+            this.ExitButton.TabIndex = 22;
+            this.ExitButton.Text = "Назад";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // OrderButton
+            // 
+            this.OrderButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.OrderButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.OrderButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.OrderButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OrderButton.Location = new System.Drawing.Point(33, 377);
+            this.OrderButton.Name = "OrderButton";
+            this.OrderButton.Size = new System.Drawing.Size(127, 41);
+            this.OrderButton.TabIndex = 23;
+            this.OrderButton.Text = "Мои заказы";
+            this.OrderButton.UseVisualStyleBackColor = false;
+            this.OrderButton.Click += new System.EventHandler(this.OrderButton_Click);
+            // 
+            // AccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NeedleWorkShop2.Properties.Resources.mainFon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(850, 448);
+            this.Controls.Add(this.OrderButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.AddressTextBox);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.PatronymicTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.CustomerButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AccountForm";
+            this.Text = "Аккаунт";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button CustomerButton;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox PatronymicTextBox;
+        private System.Windows.Forms.TextBox PhoneNumberTextBox;
+        private System.Windows.Forms.TextBox AddressTextBox;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button OrderButton;
+    }
+}
---
+++ AccountForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NeedleWorkShop2
+{
+    public partial class AccountForm : Form
+    {
+        Database db = new Database(); //подключение к бд
+        public AccountForm()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen; //расположение по центру экрана
+            bool hasAccount = Presence();  //активация метода проверки наличия аккаунта у пользователя
+        }
+        public static int ID_Customer { get; set; }
+        private Boolean Presence() //метод, проверяющий наличие резюме у пользователя
+        {
+            int userId = AuthorizationForm.ID_User;
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            string query = $"select ID_Customer, Last_Name, First_Name, Patronymic,Phone_Number, Address," +
+                $" ID_User from Customer where ID_User='{userId}'"; //выбока данных определенного пользователя
+            MySqlCommand command = new MySqlCommand(query, db.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            if (table.Rows.Count > 0) //проверка наличия данных о пользователе
+            {
+                db.OpenConnection();
+                MySqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read()) //вывод данных
+                {
+                    LastNameTextBox.Text = dataReader[1].ToString();
+                    FirstNameTextBox.Text = dataReader[2].ToString();
+                    PatronymicTextBox.Text = dataReader[3].ToString();
+                    PhoneNumberTextBox.Text = dataReader[4].ToString();
+                    AddressTextBox.Text = dataReader[5].ToString();
+                }
+                db.CloseConnection();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private Boolean Check() //метод, проверяющий чтобы все обязательные поля были заполнены
+        {
+            var lastName = LastNameTextBox.Text;
+            var firstName = FirstNameTextBox.Text;
+            var patronymic = PatronymicTextBox.Text;
+            var phoneNumber = PhoneNumberTextBox.Text;
+            var address = AddressTextBox.Text;
+            if (string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(patronymic)
+                || string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(address))
+            {
+                MessageBox.Show("Вы заполнили не все обязательные поля!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void CustomerButton_Click(object sender, EventArgs e)
+        {
+            var lastName = LastNameTextBox.Text;
+            var firstName = FirstNameTextBox.Text;
+            var patronymic = PatronymicTextBox.Text;
+            var phoneNumber = PhoneNumberTextBox.Text;
+            var address = AddressTextBox.Text;
+            int userId = AuthorizationForm.ID_User;
+            string query = $"update Customer set Last_Name = '{lastName}', First_Name = '{firstName}', " +
+                $"Patronymic = '{patronymic}', Phone_Number = '{phoneNumber}', Address = '{address}' " +
+                $"where ID_User = '{userId}'"; //данные о покупателе в базе данных
+            MySqlCommand command = new MySqlCommand(query, db.GetConnection());
+            db.OpenConnection();
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Данные сохранены!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            db.CloseConnection();
+        }
+        private void OrderButton_Click(object sender, EventArgs e)
+        {
+            int userId = AuthorizationForm.ID_User;
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            string query = $"select * from Customer where ID_User ='{userId}'"; //выборка определенного покупателя
+            MySqlCommand command = new MySqlCommand(query, db.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            if (table.Rows.Count == 1)
+            {
+                db.OpenConnection();
+                MySqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    AccountForm.ID_Customer = (int)dataReader["ID_Customer"];
+                }
+                db.CloseConnection();
+            }
+            OrderForm orderButton = new OrderForm();
+            this.Close();
+            orderButton.Show(); //переход к форме заказов
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            CustomerMainForm customerMainForm = new CustomerMainForm();
+            this.Close();
+            customerMainForm.Show(); //переход к главной форме
+        }
+    }
+}
