1b5d6b06b921eef5b3c7bb3c395dcb7746d6ce51
Юлия Агафонова <uliaagafonova6143@gmail.com>
2024-12-23 13:49:07 +0400

    Add files via upload

---
+++ AuthorizationForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace NeedleWorkShop2
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuthorizationForm));
+            this.AuthorizationButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RegistrationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AuthorizationButton
+            // 
+            this.AuthorizationButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.AuthorizationButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.AuthorizationButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.AuthorizationButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorizationButton.Location = new System.Drawing.Point(175, 325);
+            this.AuthorizationButton.Name = "AuthorizationButton";
+            this.AuthorizationButton.Size = new System.Drawing.Size(98, 35);
+            this.AuthorizationButton.TabIndex = 3;
+            this.AuthorizationButton.Text = "Войти";
+            this.AuthorizationButton.UseVisualStyleBackColor = false;
+            this.AuthorizationButton.Click += new System.EventHandler(this.AuthorizationButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(462, 52);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Авторизация";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PasswordTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(133, 257);
+            this.PasswordTextBox.Multiline = true;
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(198, 25);
+            this.PasswordTextBox.TabIndex = 10;
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginTextBox.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginTextBox.Location = new System.Drawing.Point(133, 197);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(198, 27);
+            this.LoginTextBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(51, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Пароль";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(51, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Логин";
+            // 
+            // RegistrationLabel
+            // 
+            this.RegistrationLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.RegistrationLabel.AutoSize = true;
+            this.RegistrationLabel.BackColor = System.Drawing.Color.Linen;
+            this.RegistrationLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RegistrationLabel.Font = new System.Drawing.Font("Times New Roman", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegistrationLabel.ForeColor = System.Drawing.Color.MediumBlue;
+            this.RegistrationLabel.Location = new System.Drawing.Point(149, 363);
+            this.RegistrationLabel.Name = "RegistrationLabel";
+            this.RegistrationLabel.Size = new System.Drawing.Size(213, 17);
+            this.RegistrationLabel.TabIndex = 11;
+            this.RegistrationLabel.Text = "Вы еще не зарегистрированы?";
+            this.RegistrationLabel.Click += new System.EventHandler(this.RegistrationLabel_Click);
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NeedleWorkShop2.Properties.Resources.фон3;
+            this.ClientSize = new System.Drawing.Size(462, 463);
+            this.Controls.Add(this.RegistrationLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AuthorizationButton);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AuthorizationForm";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.AuthorizationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AuthorizationButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label RegistrationLabel;
+    }
+}
+
---
+++ AuthorizationForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NeedleWorkShop2
+{
+    public partial class AuthorizationForm : Form
+    {
+        Database db = new Database(); //подключение к бд
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen; //расположение по центру экрана
+        }
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+            PasswordTextBox.PasswordChar = '*'; //скрытие пароля символом
+        }
+        public static int ID_User { get; set; }
+        private Boolean Check() //метод, проверяющий чтобы все обязательные поля были заполнены
+        {
+            var login = LoginTextBox.Text;
+            var password = PasswordTextBox.Text;
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Вы заполнили не все обязательные поля!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void AuthorizationButton_Click(object sender, EventArgs e)
+        {
+            if (Check()) //активация метода проверки пустых полей
+            {
+                return;
+            }
+            var login = LoginTextBox.Text;
+            var password = PasswordTextBox.Text;
+            var role = 0;
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            string query = $"select * from User where Login ='{login}' and Password ='{password}'"; //выборка данных из таблицы User
+            MySqlCommand command = new MySqlCommand(query, db.GetConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            db.OpenConnection();
+            MySqlDataReader RoleDataReader = command.ExecuteReader();
+            while (RoleDataReader.Read()) //нахождение роли
+            {
+                role = (int)RoleDataReader["Role"];
+            }
+            db.CloseConnection();
+            if (table.Rows.Count == 1) //проверка наличия такого пользователя
+            {
+                MessageBox.Show("Вы успешно вошли!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (role == 2)
+                {
+                    ManagerMainForm managerMainForm = new ManagerMainForm();
+                    managerMainForm.Show(); //переход к главной форме для менеджера
+                    this.Hide();
+                }
+                else
+                {
+                    db.OpenConnection();
+                    MySqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read()) //вывод задач сотрудника
+                    {
+                        AuthorizationForm.ID_User = (int)dataReader["ID_User"];
+                    }
+                    db.CloseConnection();
+                    CustomerMainForm customerMainForm = new CustomerMainForm();
+                    customerMainForm.Show(); //переход к главной форме для сотрудника
+                    this.Hide();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void RegistrationLabel_Click(object sender, EventArgs e)
+        {
+            RegistrationForm registrationForm = new RegistrationForm();
+            registrationForm.Show(); //переход к форме регистрации
+            this.Hide();
+        }
+    }
+}
